947bd4928fb8c59cfa91a6f88a408baa1477f5c1
钱恺 <sknight@qiankaideMacBook-Pro.local>
2019-02-12 22:35:53 +0800

    add a docs folder

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
