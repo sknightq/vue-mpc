479a4159d867097e94baa41d8e620678ae2b2a05
Sknight Qian <sknight127@sina.com>
2021-04-07 08:40:47 +0800

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex">
+    <div class="flex-none w-48 relative">
+      <slot name="left"></slot>
+    </div>
+    <div class="flex-auto p-6">
+      <slot name="right"></slot>
+    </div>
+  </section>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'card',
+  setup() {}
+})
+</script>
+<style lang="postcss"></style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="flex flex-row list-none ml-auto">
+    <li class="nav-item" v-for="(item, name) in items" :key="name">
+      <a class="nav-item-a" :href="item.url">{{ item.name }}</a>
+    </li>
+  </ul>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'nav',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup(props) {}
+})
+</script>
+<style lang="postcss">
+.nav-item-a {
+  @apply px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75;
+}
+</style>
