529766773dacd67d68fd45a964d61d25aa7820dc
Sknight Qian <sknight127@sina.com>
2021-04-07 08:42:43 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         'vue-green': '#42b983'
       }
-    }
-  }
+    },
+    screens: {} //自定义断点
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
 }
